bb1ebe487512b9edd042a6eebb6be8e2d41a6649
bbjansen <bobjansen@pm.me>
2020-06-10 13:09:18 +0200

    strip default vue-cli starter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Validator msg="test"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import Validator from './components/Validator.vue'
 
 @Component({
   components: {
-    HelloWorld
+    Validator
   }
 })
 export default class App extends Vue {}
@@ -19,11 +18,6 @@ export default class App extends Vue {}
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>

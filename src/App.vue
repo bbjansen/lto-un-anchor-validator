0d7d9e229cf21efe80418880ffac1a5d1eff3c99
bbjansen <bobjansen@pm.me>
2020-06-10 13:33:45 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-    <Validator msg="test"/>
+    <Navbar></Navbar>
+    <section class="hero is-primary is-medium">
+      <div class="hero-body">
+      </div>
+    </section>
+
+    <Validator msg=""/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import Navbar from './components/Navbar.vue'
 import Validator from './components/Validator.vue'
 
 @Component({
   components: {
+    Navbar,
     Validator
   }
 })
---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-navbar class="is-primary">
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="@/assets/img/lto-logo.svg"
+                    alt="LTO Network - Hybrid Blockchain"
+                >
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="@/assets/img/until-logo.svg"
+                    alt="UNTIL - United Nations Technology Innovation Labs"
+                >
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Navbar extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>

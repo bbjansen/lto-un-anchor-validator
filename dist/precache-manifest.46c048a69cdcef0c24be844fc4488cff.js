458b5a80ea1125b457370b84bc2d2f5b559cc4c4
bbjansen <bobjansen@pm.me>
2020-06-12 02:18:06 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.46c048a69cdcef0c24be844fc4488cff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821f3f4cf4f298cff47e",
+    "url": "/un/css/app.1fbb6823.css"
+  },
+  {
+    "revision": "aad0174a1d435a718081a48068a7b53f",
+    "url": "/un/img/document.aad0174a.svg"
+  },
+  {
+    "revision": "ddf665db5158eef90b057d0ebe6dbf3f",
+    "url": "/un/img/lto-logo.ddf665db.svg"
+  },
+  {
+    "revision": "b6fc09cec6850db182f6c04f55f376bc",
+    "url": "/un/img/until-logo.b6fc09ce.svg"
+  },
+  {
+    "revision": "74b54b755086526ec035899cfa458888",
+    "url": "/un/img/view-document.74b54b75.svg"
+  },
+  {
+    "revision": "8d22210b64d817d00c4acaac171299cd",
+    "url": "/un/index.html"
+  },
+  {
+    "revision": "821f3f4cf4f298cff47e",
+    "url": "/un/js/app.145837f5.js"
+  },
+  {
+    "revision": "f81834dc563def74d285",
+    "url": "/un/js/chunk-vendors.ea4efe95.js"
+  },
+  {
+    "revision": "cde4a8b52b1f59cb242bd1e1b56c4ca0",
+    "url": "/un/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/un/robots.txt"
+  }
+]);
